0a37aab37e7582b1b474c191883034b2ce0e663d
Shelton <shelton.g.w.tso@gmail.com>
2017-09-19 21:11:31 -0700

    Removed old verify page.

--- routes/verify.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET API. */
-router.get('/verify', function(req, res, next) {
-    res.render('verify');
-});
-
-module.exports = router;
